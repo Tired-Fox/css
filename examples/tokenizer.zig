000304178f55a0587af0cf0fe132090205742740
Tired-Fox <zboehm104@gmail.com>
2024-08-07 23:36:07 -0500

    [feat] Consume string & Consume escape

--- examples/tokenizer.zig
+++ examples/tokenizer.zig
@@ -2,19 +2,13 @@ const std = @import("std");
 const css = @import("css");
 
 pub fn main() !void {
-    const source = "\r\n/* Sample CSS */\r\np > a {\r\n  color: red;\r\n}\r\n";
+    const source = "\r\n/* Sample CSS */\r\n'hello \\263A'p > a {\r\n  color: red;\r\n}\r\n";
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
     var tokenizer = try css.Tokenizer.init(arena.allocator(), source);
-    if (try tokenizer.consume()) |token| {
-        std.debug.print("{any}\n", .{token});
-        std.debug.print("Span {any}\n", .{tokenizer.loc()});
-    }
-
-    if (try tokenizer.consume()) |token| {
-        std.debug.print("{any}\n", .{token});
-        std.debug.print("Span {any}\n", .{tokenizer.loc()});
+    while (try tokenizer.consume()) |token| {
+        std.debug.print("{any} {any}\n", .{ token, tokenizer.loc() });
     }
 }
--- src/token.zig
+++ src/token.zig
@@ -166,17 +166,49 @@ pub const Token = union(enum) {
     }
 };
 
+const ParseError = error{
+    UnclosedComment,
+    UnclosedString,
+    UnclosedUrl,
+
+    InvalidEscapeSequence,
+};
+
+/// Check if the code points are a valid escape sequence.
+fn isValidEscape(code_points: *[2]?u21) bool {
+    if (code_points[0] == null or code_points[1] == null) return false;
+    if (code_points[0] != '\\' or code_points[1] == '\n') return false;
+    return true;
+}
+
+fn isWhitespace(code_point: u21) bool {
+    return code_point == ' ' or code_point == '\t' or code_point == '\n';
+}
+
+fn isHexDigit(code_point: u21) bool {
+    switch (code_point) {
+        '0'...'9', 'a'...'f', 'A'...'F' => return true,
+        else => return false,
+    }
+}
+
+fn isSurrogate(code_point: u32) bool {
+    return (code_point >= 0xD800 and code_point <= 0xDBFF) or (code_point >= 0xDC00 and code_point <= 0xDFFF);
+}
+
 pub const Tokenizer = struct {
     alloc: std.mem.Allocator,
     stream: Parser,
 
     location: Span,
 
-    ident: std.ArrayList([]const u8),
-    hash: std.ArrayList([]const u8),
-    number: std.ArrayList([]const u8),
-    string: std.ArrayList([]const u8),
-    whitespace: std.ArrayList([]const u8),
+    idents: std.StringHashMap(void),
+    hashes: std.StringHashMap(void),
+    numbers: std.StringHashMap(void),
+    strings: std.StringHashMap(void),
+    whitespace: std.StringHashMap(void),
+
+    parse_errors: std.ArrayList(ParseError),
 
     const Self = @This();
 
@@ -187,12 +219,13 @@ pub const Tokenizer = struct {
         return .{
             .stream = try Parser.init(stream),
             .location = .{ .start = 0, .end = 0 },
+            .parse_errors = std.ArrayList(ParseError).init(alloc),
 
-            .ident = std.ArrayList([]const u8).init(alloc),
-            .hash = std.ArrayList([]const u8).init(alloc),
-            .number = std.ArrayList([]const u8).init(alloc),
-            .string = std.ArrayList([]const u8).init(alloc),
-            .whitespace = std.ArrayList([]const u8).init(alloc),
+            .idents = std.StringHashMap(void).init(alloc),
+            .hashes = std.StringHashMap(void).init(alloc),
+            .numbers = std.StringHashMap(void).init(alloc),
+            .strings = std.StringHashMap(void).init(alloc),
+            .whitespace = std.StringHashMap(void).init(alloc),
 
             .alloc = alloc,
         };
@@ -204,10 +237,13 @@ pub const Tokenizer = struct {
     /// Only call if you wish to free all allocated memory. Any references and uses of the token string slices
     /// will become invalid.
     pub fn deinit(self: *Self) void {
-        self.ident.deinit();
-        self.hash.deinit();
-        self.number.deinit();
-        self.string.deinit();
+        // TODO PERF: Check if the stored string slices are also freed or if only the pointers are freed
+        //      if not then iterate each key freeing the slices
+        //      - Check this by using general purpose allocator instead of arena allocator
+        self.idents.deinit();
+        self.hashes.deinit();
+        self.numbers.deinit();
+        self.strings.deinit();
         self.whitespace.deinit();
     }
 
@@ -216,6 +252,11 @@ pub const Tokenizer = struct {
     /// @return Span of the current location in the source where the last parsed token is located
     pub fn loc(self: *Self) Span {
         return self.location;
+    }
+
+    pub fn containsValidEscape(self: *Self) bool {
+        const p2 = self.stream.peek2();
+        return isValidEscape(p2);
     }
 
     /// Consume a comment if the next two code points are `/*`.
@@ -230,10 +271,12 @@ pub const Tokenizer = struct {
                     _ = self.stream.next2();
                     break;
                 } else if (peek[0] == null) {
-                    return error.CommentMissingEnd;
+                    try self.parse_errors.append(error.UnclosedComment);
+                    return;
                 } else if (peek[1] == null) {
                     _ = self.stream.next();
-                    return error.CommentMissingEnd;
+                    try self.parse_errors.append(error.UnclosedComment);
+                    return;
                 } else {
                     _ = self.stream.next();
                 }
@@ -262,8 +305,115 @@ pub const Tokenizer = struct {
         }
 
         self.location.end = self.stream.loc();
-        try self.whitespace.append(try whitespace.toOwnedSlice());
-        return .{ .whitespace = self.whitespace.items[self.whitespace.items.len - 1] };
+        const ws = (try self.whitespace.getOrPut(try whitespace.toOwnedSlice())).key_ptr.*;
+        return .{ .whitespace = ws };
+    }
+
+    /// Consume an escape sequence.
+    ///
+    /// **WARN:** Caller is responsible for freeing the returned slice.
+    ///
+    /// @return the consumed escape sequence
+    /// @throws allocation errors when the space for parsing the hex code point or byte representation
+    ///     is not available
+    pub fn consumeEscape(self: *Self) ![]const u8 {
+        const next = self.stream.next();
+        if (next) |n| {
+            switch (n) {
+                '0'...'9', 'a'...'f', 'A'...'F' => {
+                    var buffer = try std.ArrayList(u8).initCapacity(self.alloc, 6);
+                    try buffer.append(@intCast(n));
+                    for (0..5) |_| {
+                        const peek = self.stream.peek();
+                        if (peek) |p| {
+                            if (isWhitespace(p)) {
+                                _ = self.stream.next();
+                                break;
+                            } else if (isHexDigit(p)) {
+                                try buffer.append(@intCast(self.stream.next().?));
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+
+                    self.location.end = self.stream.loc();
+
+                    var buff = try buffer.toOwnedSlice();
+                    const hex = try std.fmt.parseInt(u21, buff, 16);
+                    self.alloc.free(buff);
+                    buffer.deinit();
+
+                    if (hex == 0 or hex > 0x10FFFF or isSurrogate(hex)) {
+                        return "�";
+                    } else {
+                        buff = try self.alloc.alloc(u8, try std.unicode.utf8CodepointSequenceLength(hex));
+                        _ = try std.unicode.utf8Encode(hex, buff);
+                        return buff;
+                    }
+                },
+                else => {
+                    const buffer = try self.alloc.alloc(u8, try std.unicode.utf8CodepointSequenceLength(n));
+                    _ = try std.unicode.utf8Encode(n, buffer);
+                    return buffer;
+                },
+            }
+        } else {
+            try self.parse_errors.append(error.InvalidEscapeSequence);
+            return "�";
+        }
+        switch (self.stream.next()) {}
+    }
+
+    /// Consumes a string token until the end code point is reached.
+    ///
+    /// @return A `String` or `BadString` token
+    /// @throws parse error when the string is missing the closing code point
+    pub fn consumeString(self: *Self, end: u21) !Token {
+        var string = std.ArrayList(u8).init(self.alloc);
+        while (true) {
+            const peek = self.stream.peek();
+            if (peek) |p| {
+                if (p == end) {
+                    // End of string reached
+                    _ = self.stream.next();
+                    const s = (try self.strings.getOrPut(try string.toOwnedSlice())).key_ptr.*;
+                    self.location.end = self.stream.loc();
+                    return .{ .string = s };
+                } else if (p == '\n') {
+                    // Line break, string is invalid and not closed
+                    try self.parse_errors.append(error.UnclosedString);
+                    self.location.end = self.stream.loc();
+                    return .{ .bad_string = {} };
+                } else if (p == '\\') {
+                    // Consume an escaped character regardless of what it is.
+                    // Also translates unicode escape sequences to UTF-8 code points.
+                    const p2 = self.stream.peek2();
+                    if (p2[1] == null) {
+                        continue;
+                    } else if (p2[1] == '\n') {
+                        _ = self.stream.next();
+                        string.append('\n') catch unreachable;
+                    } else if (isValidEscape(p2)) {
+                        _ = self.stream.next();
+                        const escape = try self.consumeEscape();
+                        defer self.alloc.free(escape);
+                        try string.appendSlice(escape);
+                    }
+                } else {
+                    const next = self.stream.next().?;
+                    const buff = try self.alloc.alloc(u8, try std.unicode.utf8CodepointSequenceLength(next));
+                    _ = try std.unicode.utf8Encode(next, buff);
+                    try string.appendSlice(buff);
+                    self.alloc.free(buff);
+                }
+            } else {
+                try self.parse_errors.append(error.UnclosedString);
+                const s = (try self.strings.getOrPut(try string.toOwnedSlice())).key_ptr.*;
+                self.location.end = self.stream.loc();
+                return .{ .string = s };
+            }
+        }
     }
 
     /// Consume the next token.
@@ -278,6 +428,50 @@ pub const Tokenizer = struct {
             switch (peek) {
                 '\n', '\t', ' ' => {
                     return try self.consumeWhitespace();
+                },
+                '\'' => {
+                    _ = self.stream.next();
+                    return try self.consumeString('\'');
+                },
+                '"' => {
+                    _ = self.stream.next();
+                    return try self.consumeString('"');
+                },
+                '}' => {
+                    _ = self.stream.next();
+                    return .{ .@"}" = {} };
+                },
+                '{' => {
+                    _ = self.stream.next();
+                    return .{ .@"{" = {} };
+                },
+                ':' => {
+                    _ = self.stream.next();
+                    return .{ .colon = {} };
+                },
+                ';' => {
+                    _ = self.stream.next();
+                    return .{ .semicolon = {} };
+                },
+                ',' => {
+                    _ = self.stream.next();
+                    return .{ .comma = {} };
+                },
+                '[' => {
+                    _ = self.stream.next();
+                    return .{ .@"[" = {} };
+                },
+                ']' => {
+                    _ = self.stream.next();
+                    return .{ .@"]" = {} };
+                },
+                '(' => {
+                    _ = self.stream.next();
+                    return .{ .@"(" = {} };
+                },
+                ')' => {
+                    _ = self.stream.next();
+                    return .{ .@")" = {} };
                 },
                 else => {
                     self.location.end = self.stream.loc() + 1;
